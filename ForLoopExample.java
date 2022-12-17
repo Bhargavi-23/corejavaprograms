befa7e89d82f6487c43d091541ae5d2f958b53c8
Narasimha rao Gunda <bhargavimutcherla@gmail.com>
2022-12-17 11:41:16 +0530

    added for loop example

---
+++ ForLoopExample.java
@@ -0,0 +1,16 @@
+public class ForLoopExample {
+    public static void main(String[] args){
+        System.out.println("Increment i.e ++");
+        for(int i=0;i<=10;i++) {
+            System.out.println(i);
+            }
+        System.out.println("Example of For Loop Decrement ");
+
+        for(int j=100;j>0;) {
+             j-=10;
+            System.out.println(j);
+        }
+
+        }
+    }
+
